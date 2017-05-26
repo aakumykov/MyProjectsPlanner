1ec3b1adfae7875eaad6c495c8090d7ca9f7052c
Андрей Кумыков <aakumykov@yandex.ru>
2017-05-26 13:31:12 +0700

    привязка к изменению статуса аутентификации

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -9,23 +9,51 @@ import firebase from 'firebase';
 	templateUrl: 'app.html'
 })
 export class MyApp {
-	rootPage: any = 'HomePage';
+	public rootPage: string;
+	public zone: NgZone;	
 
 	constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+		firebase.initializeApp({
+			apiKey: "AIzaSyBDhOaoRxU0TKrU6SlI4bI4j_KlEUlO9c8",
+			authDomain: "myprojectplanner-eee98.firebaseapp.com",
+			databaseURL: "https://myprojectplanner-eee98.firebaseio.com",
+			projectId: "myprojectplanner-eee98",
+			storageBucket: "myprojectplanner-eee98.appspot.com",
+			messagingSenderId: "600991054412"
+		});
+
+		this.zone = new NgZone({});
+
+		const unsubscribe = firebase.auth().onAuthStateChanged( 
+			user => {
+				this.zone.run(
+					() => {
+						if (!user) {
+							this.rootPage = 'LandingPage';
+							unsubscribe();
+						} else {
+							firebase.database()
+								.ref(`/userProfile/${firebase.auth().currentUser.uid}`)
+								.once('value', userProfile => {
+										if (userProfile.val().active === true){
+											this.rootPage = 'TabsPage';
+										} else {
+											this.rootPage = 'WaitingPage';
+										}
+									}
+								); 
+							unsubscribe();
+						}
+					}
+				);
+			}
+		);
+
+
 		platform.ready().then(() => {
 		 
 			statusBar.styleDefault();
-			splashScreen.hide();
-			
-			firebase.initializeApp({
-				apiKey: "AIzaSyBDhOaoRxU0TKrU6SlI4bI4j_KlEUlO9c8",
-				authDomain: "myprojectplanner-eee98.firebaseapp.com",
-				databaseURL: "https://myprojectplanner-eee98.firebaseio.com",
-				projectId: "myprojectplanner-eee98",
-				storageBucket: "myprojectplanner-eee98.appspot.com",
-				messagingSenderId: "600991054412"
-			});
-			
+			splashScreen.hide();			
 		});
 	}
 }
