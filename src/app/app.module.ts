37cf69e3e45fd5b4c5d4a22b3c1e289942120954
Андрей Кумыков <aakumykov@yandex.ru>
2017-05-26 09:31:33 +0700

    провайдеры

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AuthProvider } from '../providers/auth/auth';
+import { TaskProvider } from '../providers/task/task';
+import { TeamProvider } from '../providers/team/team';
 
 @NgModule({
   declarations: [
@@ -25,7 +28,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthProvider,
+    TaskProvider,
+    TeamProvider
   ]
 })
 export class AppModule {}
