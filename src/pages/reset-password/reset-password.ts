9654c99d7cf52f97c89eb7dd30274ee3b618e784
Андрей Кумыков <aakumykov@yandex.ru>
2017-05-27 15:03:54 +0700

    страница сброса пароля

--- src/pages/reset-password/reset-password.ts
+++ src/pages/reset-password/reset-password.ts
@@ -1,24 +1,44 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, AlertController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthProvider } from '../../providers/auth/auth';
+import { EmailValidator } from '../../validators/email';
 
-/**
- * Generated class for the ResetPasswordPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
-  selector: 'page-reset-password',
-  templateUrl: 'reset-password.html',
+	selector: 'page-reset-password',
+	templateUrl: 'reset-password.html',
 })
 export class ResetPasswordPage {
+	public resetPasswordForm:FormGroup;
+	constructor(public navCtrl:NavController, public alertCtrl:AlertController, 
+	public formBuilder:FormBuilder, public authProvider:AuthProvider) {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+	this.resetPasswordForm = formBuilder.group({
+		email: ['', Validators.compose([Validators.required, EmailValidator.isValid])],
+	});
+	}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ResetPasswordPage');
-  }
+	resetPassword(): void {
+	if (!this.resetPasswordForm.valid){
+		console.log(this.resetPasswordForm.value);
+	} else {
+		this.authProvider.resetPassword(this.resetPasswordForm.value.email).then( () => {
+		const alert = this.alertCtrl.create({
+			title: 'Письмо уже в пути!',
+			subTitle: 'Вам отправлено письмо с инструкциями по восстановлению пароля',
+			buttons: [
+			{
+				text: 'Хорошо',
+				handler: data => {
+				this.navCtrl.pop();
+				}
+			}
+			]
+		});
+		alert.present();
+		});
+	}
+	}
 
 }
