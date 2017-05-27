580b6a397165820de0ebd42298a7b3effdefeb23
Андрей Кумыков <aakumykov@yandex.ru>
2017-05-27 09:05:28 +0700

    Страница входа (проблема с TabsPage сохраняется)

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,18 +1,45 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, LoadingController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthProvider } from '../../providers/auth/auth';
+import { EmailValidator } from '../../validators/email';
 
 @IonicPage()
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html',
+	selector: 'page-login',
+	templateUrl: 'login.html',
 })
 export class LoginPage {
+	public loginForm:FormGroup;
+	constructor(public navCtrl:NavController, public loadingCtrl:LoadingController, 
+	public formBuilder:FormBuilder, public authProvider:AuthProvider) {
+		this.loginForm = formBuilder.group({
+		email: ['', Validators.compose([Validators.required, EmailValidator.isValid])],
+		password: ['', Validators.compose([Validators.minLength(6), Validators.required])]
+		});
+	}
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+	goToResetPassword(): void {
+	this.navCtrl.push('ResetPasswordPage');
+	}
 
-  ionViewDidLoad() {
-	console.log('ionViewDidLoad LoginPage');
-  }
+	loginUser(): void {
+	const loading = this.loadingCtrl.create();
+	if (!this.loginForm.valid){
+		console.log(this.loginForm.value);
+	} else {
+		this.authProvider.loginUser(this.loginForm.value.email, this.loginForm.value.password)
+		.then( activeUser => {
+		loading.dismiss().then( () => {
+			if (activeUser === true){
+			this.navCtrl.setRoot('TabsPage');
+			} else {
+			this.navCtrl.setRoot('WaitingPage');
+			}
+		})
+		});
+	}
+	loading.present();
+	}
 
 }
