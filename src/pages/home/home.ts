21001acba598b68b0ed5b0ff96386bdb6d73f010
Андрей Кумыков <aakumykov@yandex.ru>
2017-05-30 11:44:24 +0700

    Список заданий работает.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController } from 'ionic-angular';
+import { IonicPage, NavController, AlertController } from 'ionic-angular';
 import { AuthProvider } from '../../providers/auth/auth';
 import { TeamProvider } from '../../providers/team/team';
+import { TaskProvider } from '../../providers/task/task';
 
 @IonicPage()
 @Component({
@@ -10,14 +11,28 @@ import { TeamProvider } from '../../providers/team/team';
 })
 export class HomePage {
 	public isAdmin: boolean = false;
+	public taskList: Array<any> = [];
+	public userProfile: Object = {};
 
 	constructor(
 		public navCtrl: NavController, 
+		public alertCtrl: AlertController,
 		private authProvider: AuthProvider,
-		private teamPrivider: TeamProvider
-	) {
-		this.teamPrivider.getAdminStatus().then( adinStatus => {
+		private teamProvider: TeamProvider,
+		private taskProvider: TaskProvider,
+	) {}
+
+	ionViewDidEnter(): void {
+		this.teamProvider.getAdminStatus().then( adinStatus => {
 			this.isAdmin = adinStatus;
+		});
+
+		this.teamProvider.getUserProfile().then( profile => {
+			this.userProfile = profile;
+		});
+
+		this.taskProvider.getTaskList().then( list => {
+			this.taskList = list;
 		});
 	}
 
@@ -32,5 +47,25 @@ export class HomePage {
 		this.navCtrl.push('TaskCreatePage');
 	}
 
-
+	completeTask(task): void {	
+		let confirm = this.alertCtrl.create({
+			title: 'Выполнили?',
+			message: 'Нажмите Да, чтобы пометить задание завершённым',
+			buttons: [
+				{
+					text: 'Да',
+					handler: () => {
+						this.taskProvider.completeTask(task.taskId).then( () => {
+						task.completed = true;
+						});
+					}
+				},
+				{
+					text: 'Нет',
+					role: 'cancel'
+				}
+			]
+		});
+		confirm.present();
+	}
 }
