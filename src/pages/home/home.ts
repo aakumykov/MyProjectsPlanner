71bad30b5811113b3c872bdea158f533e3ce4a6c
Андрей Кумыков <aakumykov@yandex.ru>
2017-05-30 11:21:39 +0700

    *

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
 import { AuthProvider } from '../../providers/auth/auth';
+import { TeamProvider } from '../../providers/team/team';
 
 @IonicPage()
 @Component({
@@ -8,16 +9,28 @@ import { AuthProvider } from '../../providers/auth/auth';
 	templateUrl: 'home.html',
 })
 export class HomePage {
+	public isAdmin: boolean = false;
 
-	constructor(public navCtrl: NavController, private authProvider: AuthProvider) {
-
+	constructor(
+		public navCtrl: NavController, 
+		private authProvider: AuthProvider,
+		private teamPrivider: TeamProvider
+	) {
+		this.teamPrivider.getAdminStatus().then( adinStatus => {
+			this.isAdmin = adinStatus;
+		});
 	}
 
 	logOut() {
-		console.info('HomePage.logOut()');
 		this.authProvider.logoutUser().then( arg => {
 			this.navCtrl.setRoot('LandingPage');
 		});
 	}
+
+	addTask() {
+		// if (this.isAdmin) this.navCtrl.push('TaskCreatePage');
+		this.navCtrl.push('TaskCreatePage');
+	}
+
 
 }
